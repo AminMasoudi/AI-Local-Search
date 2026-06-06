617addf7f35437c8f95faa29447a0bff6b1afe96
amin masoudi <aminmasoudi2003@gmail.com>
2026-06-06 13:05:11 +0330

    Hill Climbing

--- search/hill_climbing.py
+++ search/hill_climbing.py
@@ -1,17 +1,21 @@
+from functools import lru_cache
+
+from env.utils import random_position
 from search.local_search_base import LocalSearchBase
+from search.state import State
 
 
 class HillClimbing(LocalSearchBase):
     def run(self, initial_state, **kwargs):
         """
         TODO: Implement the Hill Climbing algorithm.
-        
+
         Parameters
         ----------
         initial_state : list of tuples
             The initial configuration of sensors.
-        **kwargs : 
-            Define and add any other parameters you might need for the algorithm 
+        **kwargs :
+            Define and add any other parameters you might need for the algorithm
 
         Returns
         -------
@@ -23,6 +27,25 @@ class HillClimbing(LocalSearchBase):
             List of costs at each iteration (used for plotting).
         states_history : list of lists
             List of states at each iteration (used for animation).
-        """
-        
-        raise NotImplementedError("Students must implement this method.")
+        """        
+        current = initial_state
+        self.state_history = [list(current.locations)]
+        cost_history = [self.heuristic(current)]
+        i = 0
+        while i < 100:
+            successors = self.get_neighbor(current)
+            new_state = max(successors, key=lambda x: self.heuristic(x))
+            if cost_history[-1] > self.heuristic(new_state) or cost_history[-1] == 100:
+                break
+            current = new_state
+            self.state_history.append(list(current.locations))
+            cost_history.append(self.heuristic(current))
+            i += 1
+        return (
+            self.state_history[-1],
+            self.evaluate(self.heuristic(current)),
+            list(map(lambda x: self.evaluate(x), cost_history)),
+            self.state_history,
+        )
+
+
--- search/local_search_base.py
+++ search/local_search_base.py
@@ -1,40 +1,109 @@
+from abc import ABC, abstractmethod
+from functools import lru_cache
+
+from env.grid_world import GridWorld
+from env.utils import random_position
+from search.state import State
+
+
 class LocalSearchBase:
+
+    world: GridWorld
+
     def __init__(self, world):
         self.world = world
 
-    def evaluate(self, state):
+    def evaluate(self, score: float):
         """
         TODO: Implement the evaluation (Cost) function.
-        
+
         Design a function that calculates the cost of the current sensor placement.
         Refer to the project documentation for the primary objectives and constraints.
-        
-        Returns:    
+
+        Returns:
             cost (int or float): The evaluated cost of the state (lower is better).
         """
-        raise NotImplementedError("Students must implement this method.")
+        return 100 - score
 
-    def get_neighbor(self, state):
+    def get_neighbor(self, state: State) -> set[State]:
         """
         TODO: Implement the neighbor generation function.
-        
+
         Generate a new valid state by applying a local change to the current state.
         Ensure you include all the required operations mentioned in the project PDF
         to support a dynamic search space.
-        
+
         Returns:
             neighbor_state (list of tuples): The newly generated valid state.
         """
-        raise NotImplementedError("Students must implement this method.")
+        neigh_states = set()
+        for x, y in state.locations:
+            other_locations = set(
+                filter(lambda loc: not (loc[0] == x and loc[1] == y), state.locations)
+            )
 
-    def initialize_state(self):
+            new_neigh_states = set(
+                [
+                    State(
+                        locations=frozenset(other_locations.union({new_loc})),
+                    )
+                    for new_loc in State.covered_point(x, y, self.world.sensor_range)
+                    if new_loc not in other_locations
+                    and self.world.is_valid_position(new_loc[0], new_loc[1])
+                ]
+            )
+            new_neigh_states = set(
+                filter(
+                    lambda new: list(new.locations) not in self.state_history,
+                    new_neigh_states,
+                )
+            )
+            neigh_states = neigh_states.union(new_neigh_states)
+
+        if self.world.max_sensors > len(state.locations):
+            new_sensor_loc = max(
+                set(filter(lambda loc: loc not in state.locations, self.grid_world())),
+                key=lambda loc: self.point_heuristic(loc[0], loc[1], state),
+            )
+            neigh_states.add(
+                State(locations=state.locations.union({new_sensor_loc}))
+            )
+        return neigh_states
+
+    @staticmethod
+    def initialize_state(world: GridWorld):
         """
-        TODO: Generate a valid initial state.
-        
         Create a starting configuration of sensors within the grid boundaries,
         respecting the maximum sensor limits and obstacle placements.
-        
+
         Returns:
             initial_state (list of tuples): The starting coordinates of the sensors.
         """
-        raise NotImplementedError("Students must implement this method.")
+        return State(locations=frozenset([random_position(world)]))
+
+    def heuristic(self, state: State) -> float:
+        targets = self.world.get_targets()
+        covered = state.covered(self.world)
+        return len(list(filter(lambda x: x in covered, targets))) * 100 / len(targets)
+
+    @lru_cache
+    def point_heuristic(self, x: int, y: int, state: State) -> int:
+        covered = State.covered_point(x, y, self.world.sensor_range)
+        targets = self.world.get_targets()
+        return len(
+            list(
+                filter(
+                    lambda x: x in covered and x not in state.covered(self.world),
+                    targets,
+                )
+            )
+        )
+
+    @lru_cache
+    def grid_world(self):
+        return [
+            (i, j)
+            for i in range(self.world.rows)
+            for j in range(self.world.cols)
+            if self.world.is_valid_position(i, j)
+        ]
